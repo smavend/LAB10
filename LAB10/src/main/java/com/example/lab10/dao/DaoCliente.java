6f5888da0dc4518f7465352581554a48e6c8d764
amanr <amanrique068@gmail.com>
2022-11-16 21:20:59 -0500

    aa

--- LAB10/src/main/java/com/example/lab10/dao/DaoCliente.java
+++ LAB10/src/main/java/com/example/lab10/dao/DaoCliente.java
@@ -3,5 +3,7 @@ package com.example.lab10.dao;
 import com.example.lab10.bean.Cliente;
 
 public class DaoCliente {
-    public Cliente buscarCliente()
+    public Cliente buscarCliente(String numDocumento){
+        
+    }
 }
