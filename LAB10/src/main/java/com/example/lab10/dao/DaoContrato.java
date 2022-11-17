6316a35f0827cd276be71bc4454bf71f1d1de49b
amanr <amanrique068@gmail.com>
2022-11-16 20:47:44 -0500

    beans completos

--- LAB10/src/main/java/com/example/lab10/dao/DaoContrato.java
+++ LAB10/src/main/java/com/example/lab10/dao/DaoContrato.java
@@ -1,4 +1,14 @@
 package com.example.lab10.dao;
 
+import com.example.lab10.bean.Contrato;
+
+import java.util.ArrayList;
+
 public class DaoContrato {
+    public ArrayList<Contrato> listarContratos(String idCliente){
+        ArrayList<Contrato> lista = new ArrayList<>();
+
+
+        return lista;
+    }
 }
