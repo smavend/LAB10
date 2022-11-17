a044c6f6315744a73ee91debbf69c5199f55f71c
uwu <si@pucp.edu.pe>
2022-11-16 21:20:39 -0500

    login

--- LAB10/src/main/java/com/example/lab10/dao/DaoCredencial.java
+++ LAB10/src/main/java/com/example/lab10/dao/DaoCredencial.java
@@ -1,5 +1,7 @@
 package com.example.lab10.dao;
 import java.sql.*;
+
+import com.example.lab10.bean.Cliente;
 import com.example.lab10.bean.Credencial;
 
 public class DaoCredencial extends DaoBase{
@@ -28,5 +30,30 @@ public class DaoCredencial extends DaoBase{
         }
         return credencial;
     }
+
+    public boolean createCredentialCliente (Credencial credencial) {
+
+        String sql = "INSERT INTO `lab9`.`arbitro` (`nombre`, `pais`) VALUES (?, ?);";
+        DaoArbitros daoArbitros = new DaoArbitros();
+        for (Arbitro a : daoArbitros.listarArbitros()){
+            if(a.getNombre().equalsIgnoreCase(arbitro.getNombre())){
+                return false;
+            }
+        }
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            pstmt.setString(1, arbitro.getNombre());
+            pstmt.setString(2, arbitro.getPais());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException throwables){
+            return false;
+        }
+        return true;
+    }
+
+
 
 }
--- LAB10/src/main/java/com/example/lab10/servlet/ServletLogin.java
+++ LAB10/src/main/java/com/example/lab10/servlet/ServletLogin.java
@@ -6,15 +6,31 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "ServletLogin", value = "/ServletLogin")
+@WebServlet(name = "ServletLogin", urlPatterns = {"/ServletLogin","/"})
 public class ServletLogin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String action = request.getParameter("action") == null ?
+                "loginform" : request.getParameter("action");
+
+        RequestDispatcher view;
+
+        switch (action) {
+            case "loginform":
+                view = request.getRequestDispatcher("login.jsp");
+                view.forward(request, response);
+                break;
+
+
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws
+    ServletException, IOException {
+
+        
 
     }
 }
