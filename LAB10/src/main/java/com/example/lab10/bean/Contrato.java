1983c2b4bc3ec24d08b6927f7bda185d576eb684
amanr <amanrique068@gmail.com>
2022-11-16 20:37:25 -0500

    beans completos

--- LAB10/src/main/java/com/example/lab10/bean/Contrato.java
+++ LAB10/src/main/java/com/example/lab10/bean/Contrato.java
@@ -1,6 +1,49 @@
 package com.example.lab10.bean;
 
 public class Contrato {
-    String nroContrato;
+    private String nroContrato;
+    private Cliente cliente;
+    private int divisa;
+    private int estado;
+    private int mesesEnEstado;
 
+    public String getNroContrato() {
+        return nroContrato;
+    }
+
+    public void setNroContrato(String nroContrato) {
+        this.nroContrato = nroContrato;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getDivisa() {
+        return divisa;
+    }
+
+    public void setDivisa(int divisa) {
+        this.divisa = divisa;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getMesesEnEstado() {
+        return mesesEnEstado;
+    }
+
+    public void setMesesEnEstado(int mesesEnEstado) {
+        this.mesesEnEstado = mesesEnEstado;
+    }
 }
---
+++ LAB10/src/main/java/com/example/lab10/bean/Credencial.java
@@ -0,0 +1,22 @@
+package com.example.lab10.bean;
+
+public class Credencial {
+    private String numDocumento;
+    private int tipoUsuario;
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public void setNumDocumento(String numDocumento) {
+        this.numDocumento = numDocumento;
+    }
+
+    public int getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(int tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
